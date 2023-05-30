1eabe9547ea35f5749f8d2e8a148e7ac7b076364
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2023-05-31 03:19:51 +0900

    test

--- www/Typescript/tsx/components/ichi.tsx
+++ www/Typescript/tsx/components/ichi.tsx
@@ -9,7 +9,8 @@ const renewSensorData = () => {
 }*/
 
 export const IchiWidgetHead = () => {
-    const [sensorData, setSensorData] = useState([0,0,0])
+    const [sensorData, setSensorData] = useState([0, 0, 0])
+    const [countData, setCountData] = useState(0)
     const browserData = () => {
         return "ブラウザ⇒" + window.navigator.userAgent;
     }
@@ -17,13 +18,14 @@ export const IchiWidgetHead = () => {
         window.addEventListener("devicemotion", (dat) => {
             setSensorData([dat.accelerationIncludingGravity.x, dat.accelerationIncludingGravity.y, dat.accelerationIncludingGravity.z])
         });
+        setCountData(countData+1)
     };
     return (
         <div id="Ichi-widget-head">
             <div className="row p-1 px-3 ftcol">
                 <div className="col-sm-12 col-lg-8 p-1">
                     <div className="d-flex justify-content-center justify-content-lg-start">
-                        <a className="a-nolink" onClick={(evt) => { window.location.href = "https://tech-study-group.connpass.com/event/187008/" }}>
+                        <a className="a-nolink" onClick={(evt) => { window.location.href = "https://pby-tlnesjcoqq-an.a.run.app/" }}>
                             <h2 className="slidein-2 btn-push" style={{ fontFamily: "Impact", color: "indigo" }}>
                                 <i className="fas fa-book mr-1"></i>工事中
                             </h2>
@@ -41,7 +43,7 @@ export const IchiWidgetHead = () => {
                     <div className="d-flex justify-content-center" style={{ fontFamily: "Courier", color: "darkslategray" }}>
                         情報<br />
                         {browserData()}<br />
-                        {"加速度センサー⇒" + "x=" + sensorData[0] + " y=" + sensorData[1] + " z=" + sensorData[2]}<br />
+                        {"加速度センサー⇒" + "x=" + sensorData[0] + " y=" + sensorData[1] + " z=" + sensorData[2]+ " C=" + countData}<br />
                     </div>
                     <button className="input-group-append btn btn-outline-primary btn-lg" id="index_kensaku_button"
                         onClick={() => { renewSensorData() }}>
