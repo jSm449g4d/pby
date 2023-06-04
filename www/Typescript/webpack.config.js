b35307dfe1ce183ca099e257306f2c7bf7420739
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2023-06-04 23:10:23 +0900

    npm update 3

--- www/Typescript/webpack.config.js
+++ www/Typescript/webpack.config.js
@@ -30,5 +30,9 @@ module.exports = {
   externals: {
     react: "React",
     "react-dom": "ReactDOM",
+    'recharts':'Recharts',
+    'moment':'moment',
+    'crypto-js':'CryptoJS',
+    'js-cookie':'Cookies',
   },
 };
